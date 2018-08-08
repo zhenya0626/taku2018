c2e1a771cbd9ccc38203c110161bf7fb531e30c9
内間理亜奈 <riana0626@yahoo.co.jp>
2018-08-08 17:53:47 +0900

    fix building show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @current_user = current_user
+    if @current_user.location_id == nil
+      @current_user.location_id = 1
+      @current_user.save
+    end
     @users = User.all
     for @user in @users do
       @user.location = Location.find(@user.location_id)
