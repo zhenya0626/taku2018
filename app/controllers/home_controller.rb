6863f4f845e391d03dd092e1187cedbf9f5cb479
内間理亜奈 <riana0626@yahoo.co.jp>
2018-08-08 10:30:38 +0900

    fix-map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    for @user in @users do
+      @user.location = Location.find(@user.location_id)
+    end
+    @location = Location.find(current_user.location_id)
+    @location_room = @location.room
   end
 end
